dc1fb2e16edd69d01f031406df84606df8d9e580
ziro <palembani@gmail.com>
2021-03-31 13:20:08 +0700

    - Removed inventory from MasterBlockEntity

--- src/main/java/com/null2264/storagenetwork/block/entity/MasterBlockEntity.java
+++ src/main/java/com/null2264/storagenetwork/block/entity/MasterBlockEntity.java
@@ -1,22 +1,11 @@
 package com.null2264.storagenetwork.block.entity;
 
-import com.null2264.storagenetwork.api.ImplementedInventory;
 import com.null2264.storagenetwork.registry.BlockEntityRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.inventory.Inventories;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.util.collection.DefaultedList;
 
-public class MasterBlockEntity extends BlockEntity implements ImplementedInventory {
-    private final DefaultedList<ItemStack> items = DefaultedList.ofSize(2, ItemStack.EMPTY);
-
-    @Override
-    public DefaultedList<ItemStack> getItems() {
-        return items;
-    }
-
+public class MasterBlockEntity extends BlockEntity{
     public MasterBlockEntity() {
         super(BlockEntityRegistry.MASTER_BLOCK_ENTITY);
     }
@@ -24,12 +13,10 @@ public class MasterBlockEntity extends BlockEntity implements ImplementedInvento
     @Override
     public void fromTag(BlockState state, CompoundTag tag) {
         super.fromTag(state, tag);
-        Inventories.fromTag(tag,items);
     }
 
     @Override
     public CompoundTag toTag(CompoundTag tag) {
-        Inventories.toTag(tag,items);
         return super.toTag(tag);
     }
 }
