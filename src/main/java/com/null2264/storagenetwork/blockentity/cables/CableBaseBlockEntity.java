4b75ee663e87fed073e1a573df5970cae9c3495b
ziro <palembani@gmail.com>
2021-04-04 14:32:05 +0700

    + Added CableBaseBlockEntity

--- src/main/java/com/null2264/storagenetwork/blockentity/cables/CableBaseBlockEntity.java
+++ src/main/java/com/null2264/storagenetwork/blockentity/cables/CableBaseBlockEntity.java
@@ -1,7 +1,6 @@
 package com.null2264.storagenetwork.blockentity.cables;
 
 import com.null2264.storagenetwork.api.DimPos;
-import com.null2264.storagenetwork.block.cables.CableBlock;
 import com.null2264.storagenetwork.registry.BlockEntityRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
@@ -11,10 +10,25 @@ import net.minecraft.util.math.BlockPos;
 
 import java.util.Objects;
 
-public class CableBlockEntity extends CableBaseBlockEntity
+public class CableBaseBlockEntity extends BlockEntity
 {
-    public CableBlockEntity() {
+    public CableBaseBlockEntity(BlockEntityType<?> type) {
         super(BlockEntityRegistry.CABLE_BLOCK_ENTITY);
+    }
+
+    public DimPos storagePos = null;
+
+    public boolean isValidInventory(BlockPos blockPos) {
+        BlockPos invalidPos = new BlockPos(0, 0, 0);
+        return !Objects.equals(blockPos, invalidPos);
+    }
+
+    public boolean hasInventory() {
+        /* Check if link cable already has inventory */
+        if (storagePos == null)
+            return false;
+        BlockPos pos = storagePos.getBlockPos();
+        return isValidInventory(pos);
     }
 
     @Override
--- src/main/java/com/null2264/storagenetwork/blockentity/cables/CableLinkBlockEntity.java
+++ src/main/java/com/null2264/storagenetwork/blockentity/cables/CableLinkBlockEntity.java
@@ -3,26 +3,12 @@ package com.null2264.storagenetwork.blockentity.cables;
 import com.null2264.storagenetwork.api.DimPos;
 import com.null2264.storagenetwork.registry.BlockEntityRegistry;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.util.math.BlockPos;
 
-import java.util.Objects;
-
-public class CableLinkBlockEntity extends BlockEntity
+public class CableLinkBlockEntity extends CableBaseBlockEntity
 {
-    private boolean hasMaster;
-    private DimPos storagePos;
-
     public CableLinkBlockEntity() {
         super(BlockEntityRegistry.CABLE_LINK_BLOCK_ENTITY);
-    }
-
-    public boolean hasInventory(DimPos dimPos) {
-        /* Check if link cable already has inventory */
-        BlockPos pos = dimPos.getBlockPos();
-        BlockPos invalidPos = new BlockPos(0, 0, 0);
-        return !Objects.equals(pos, invalidPos);
     }
 
     @Override
@@ -33,7 +19,7 @@ public class CableLinkBlockEntity extends BlockEntity
 
     @Override
     public CompoundTag toTag(CompoundTag tag) {
-        if (storagePos != null)
+        if (hasInventory())
             storagePos.toTag(tag);
         return super.toTag(tag);
     }
